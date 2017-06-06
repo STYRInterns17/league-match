02cdfbd70f1323822c7c6bab44ff70cc0945412c
uzeio <nikola@uzelac.io>
2017-06-06 13:15:57 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ app/src/BasePage.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by Nikola Uzelac on 6/6/2017.
+ */
+
+import * as tabris from "tabris";
+
+export abstract class BasePage{
+    public page: tabris.Page;
+
+    public abstract navigationTo(): void;
+
+    public find(selector: string) {
+        return this.page.find(selector);
+    }
+
+    public resolve(): any[] {
+        return [];
+    }
+
+    protected runResolve(): any {
+        if (this.resolve) {
+            let param = this.resolve();
+            return this.resolver(param);
+        }
+        return false;
+    }
+
+    private resolver(param: any[]): any {
+        return Promise.all(<any>param).catch((err) => {
+            console.error("Failed to resolve", err);
+        });
+    }
+}
