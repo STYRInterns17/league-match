4a2f8583b3e6cc7b02f347b4fe29ae5a4447159f
Michael Rojas <mikerojaswa@gmail.com>
2017-06-07 09:23:07 -0700

    Add assets

--- app/src/HomePage.ts
+++ app/src/HomePage.ts
@@ -31,12 +31,10 @@ export class HomePage extends BasePage {
         drawer.enabled = true;
 
         drawer.on('open', function() {
-            console.log('drawer opened');
         }).on('close', function() {
-            console.log('drawer closed');
         });
         //CREATE BUTTONS
-        let profileButton = new customButton({top: 'prev() 10'}, 'Profile');
+        let profileButton = new customButton({top: 'prev() 10'}, 'Profile').on('tap', () => console.log('Profile Button Tapped'));
         profileButton.appendTo(drawer);
 
         let notificationButton = new customButton({top: 'prev() 30'}, 'Notifications');
@@ -44,12 +42,12 @@ export class HomePage extends BasePage {
 
         let leagueButton = new customButton({top: 'prev() 30'}, 'Leagues');
         leagueButton.appendTo(drawer);
-
-        let adminButton = new customButton({top: 'prev() 30'}, 'Administrator');
+        //Add admin verification method here:
+        let adminButton = new customButton({top: 'prev() 30'}, 'Administrator').on('tap', () => console.log('Admin Button Tapped'));
         adminButton.appendTo(drawer);
 
         let leaderBoard = new Leaderboard();
-        let collectionViewLeader = leaderBoard.createLeaderBoard();
+        let collectionViewLeader = leaderBoard.createLeaderBoard([['Michael', 1200, 'avatar1.png'],['Nick', 4, 'avatar1.png'],['Curt', 1200, 'avatar1.png'],['Sal', 9000, 'avatar1.png'], ['Santa', 1200, 'avatar1.png'], ['Rudolph', 1400, 'avatar1.png'], ['Gus', 900, 'avatar1.png'], ['Octocat', 400, 'avatar2.png']]);
         this.page.append(collectionViewLeader);
 
     }
--- app/src/Leaderboard.ts
+++ app/src/Leaderboard.ts
@@ -1,36 +1,56 @@
 /**
  * Created by STYRLabs2 on 6/6/2017.
  */
-const {CollectionView, Composite, TextView} = require('tabris');
-
-let users = [['Michael', 1200],['Nick', 4],['Curt', 1200],['Sal', 9000], ['Santa', 1200], ['Rudolph', 1400], ['Gus', 900], ['Octocat', 400]].map(([name, mmr]) => ({name, mmr}));
-
-
+const {CollectionView, Composite, TextView, ImageView} = require('tabris');
+const IMAGE_PATH = 'assets/';
 export class Leaderboard{
-    public createLeaderBoard(): tabris.CollectionView{
+
+    public createLeaderBoard(users: Array): tabris.CollectionView{
+        let people = users.map(([name, mmr, image]) => ({name, mmr, image: IMAGE_PATH + image }));
+
+        function bubbleSortByMMR(a: Array)
+        {
+            let swapped;
+            do {
+                swapped = false;
+                for (let i = 0; i < a.length - 1; i++) {
+                    if (a[i].mmr < a[i + 1].mmr) {
+                        let temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+        bubbleSortByMMR(people);
+
         let collectionView = new CollectionView({
             left: 0, top: 40, right: 0, bottom: 0,
-            itemCount: users.length,
-            cellHeight: 100,
+            itemCount: people.length,
+            cellHeight: 135,
             createCell: () => {
                 let cell = new Composite();
-                // new ImageView({
-                //     top: 16, centerX: 0, width: 200, height: 200
-                // }).appendTo(cell);
+                let imageView = new ImageView({
+                    top: 16, width: 100, height: 100, right: 40
+                }).appendTo(cell);
                 new TextView({
-                    left: 30, top: 16, right: 30,
+                    left: 30, top: 50, right: [imageView, 10],
                     alignment: 'center',
                     font: 'bold 20px'
                 }).appendTo(cell);
                 return cell;
             },
             updateCell: (cell, index) => {
-                let person = users[index];
+                let person = people[index];
                 cell.apply({
-                    TextView: {text: person.name + ' ' + person.mmr},
+                    ImageView: {image: person.image},
+                    TextView: {text: person.name + ' ' + person.mmr}
                 });
             }
-        }).on('select', ({index}) => console.log('selected', users[index].name));
+        }).on('select', ({index}) => console.log('selected', people[index].name));
         return collectionView;
     }
+
 }
