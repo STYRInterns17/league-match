ae0b7d6332b1e651c1e802de15abc870f9fb389a
Curtis Sapp <curt@styr.com>
2017-06-08 16:39:47 -0700

    Added ColorScheme

---
+++ app/src/ColorScheme.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by STYR-Curt on 6/8/2017.
+ */
+export class ColorScheme {
+    public static Secondary: string = '#BDD5EA';
+    public static Background: string =  '#495867';
+    public static WigetBackground: string =  '#F7F7FF';
+    public static Accent: string =  '#FE5F55';
+    public static Primary: string =  '#577399';
+
+    // I find the 'monospace' font family look alright in most cases
+}
