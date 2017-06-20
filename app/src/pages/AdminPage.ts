f5b9390315e35c570963bda4c2ae743c6f7b14b9
Curtis Sapp <curt@styr.com>
2017-06-20 13:43:36 -0700

    Reformatted admin page layout to make it less bad

--- app/src/pages/AdminPage.ts
+++ app/src/pages/AdminPage.ts
@@ -4,6 +4,9 @@ import {Composite} from "tabris";
 import {User} from "../../../common/User";
 import {ServiceLayer} from "../ServiceLayer";
 import {ColorScheme} from "../ColorScheme";
+import {League} from "../../../common/League";
+import {customButton} from "../customButton";
+import {LeaguePreferences} from "../../../common/LeaguePreferences";
 
 /**
  * Created by STYRLabs2 on 6/7/2017.
@@ -14,23 +17,40 @@ export class AdminPage extends BasePage {
     private leagueID: number;
     private userID: number;
 
+    private league: League;
+
     constructor(leagueIdentification: number, userIdentification: number) {
         super();
         this.leagueID = leagueIdentification;
         this.userID = userIdentification;
 
-        this.createAdminPage();
+        this.getLeague().then(league => {
+            this.league = league;
+            this.createComponents();
+        })
+
     }
-    private createAdminPage() {
-        this.page.title = 'Broadcast Panel';
+
+    private createComponents() {
+        this.page.title = 'League Settings';
         this.page.background = ColorScheme.Primary
+
+
+        let leagueTitle = new tabris.TextInput({
+            top: 'prev() 20', left: '10%', right: '10%', height: 60,
+            message: 'League Title',
+            enterKeyType: 'done',
+            autoCorrect: true,
+            text: this.league.pref.title
+
+        }).appendTo(this.page);
+
         new tabris.TextInput({
-            top: 20, left: '10%', right: '10%', height: 75,
-            message: 'Send a league broadcast',
+            top: 'prev() 20', left: '10%', right: '10%', height: 60,
+            message: 'Broadcast to the league',
             enterKeyType: 'send',
             autoCorrect: true
         }).on('accept', (event) => {
-            console.log(event);
             let currentUser: User = JSON.parse(localStorage.getItem('userObj'));
             let broadcastRequest = {
                 leagueId: localStorage.getItem('currentLeagueId'),
@@ -42,7 +62,82 @@ export class AdminPage extends BasePage {
                 window.plugins.toast.showShortCenter(response.message);
             }));
         }).appendTo(this.page);
+
+        let switchComp = new Composite({
+            top: 'prev() 40',
+            left: '10%',
+            right: '10%',
+            height: 60,
+            cornerRadius: 5,
+            background: ColorScheme.Accent,
+
+        }).appendTo(this.page);
+
+        new tabris.TextView({
+            text: 'Matches must be approved by an admin',
+            left: 2,
+            right: '20%',
+            top: 0,
+            bottom: 0,
+            font: 'bold 20px',
+            textColor: '#ffffff',
+            alignment: 'center'
+
+        }).appendTo(switchComp);
+
+        let approvedByadminSwitch = new tabris.Switch({
+            left: 'prev() 10',
+            top: 0,
+            bottom: 0,
+            right: 2,
+            checked: this.league.pref.matchesApprovedByAdmin
+        }).appendTo(switchComp);
+
+
+        new customButton({
+            top: 'prev() 80',
+            left: '10%',
+            right: '10%',
+            background: ColorScheme.Accent
+        }, 'Invite Players').on('tap', (event) => {
+
+            // Open invite player page
+
+        }).changeBorderColor(ColorScheme.Primary).appendTo(this.page);
+
+        new customButton({
+            top: 'prev() 80',
+            left: '10%',
+            right: '10%',
+            background: ColorScheme.Accent
+        }, 'Apply').on('tap', (event) => {
+
+            let newPref = new LeaguePreferences(approvedByadminSwitch.checked, leagueTitle.text, this.league.pref.timeZone, this.league.pref.scoreRange, this.league.pref.highestScore)
+            let updateRequest = {leagueId: this.leagueID, leaguePref: newPref};
+            ServiceLayer.httpPostAsync('/league/update', updateRequest, response => {
+                console.log(response);
+                window.plugins.toast.showShortCenter(response.message);
+            })
+        }).changeBorderColor(ColorScheme.Primary).appendTo(this.page);
+
+
+        /*new tabris.Button({
+         top: 'prev() 50',
+         left: '10%',
+         right: '10%',
+         text: 'DELETE LEAGUE',
+         background: '#cb2431'
+         }).appendTo(this.page);*/
         return this.page;
+    }
+
+    private getLeague(): Promise<League> {
+        let p = new Promise((resolve, reject) => {
+            ServiceLayer.httpGetAsync('/league', 'leagueId=' + localStorage.getItem('currentLeagueId'), (response) => {
+                resolve(response);
+            });
+        });
+        return p;
     }
 
 }
